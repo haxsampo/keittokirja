05d4662d83840317bda5960ee9a5547a4fc47274
sampo <sampo.hakola@helsinki.fi>
2020-03-25 02:09:18 +0200

    modelien abstraktointia aloitettu; updateformit toimii

--- application/reseptit/forms.py
+++ application/reseptit/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, validators
 
-class TaskForm(FlaskForm):
+class ReseptiForm(FlaskForm):
     name = StringField("Resepti name", [validators.Length(min=2)])
     cooktime = IntegerField("Resepti cooktime")
 
--- application/reseptit/views.py
+++ application/reseptit/views.py
@@ -2,7 +2,7 @@ from flask import render_template, request, url_for, redirect
 from flask_login import login_required, current_user
 
 from application import app, db
-from application.reseptit.forms import TaskForm
+from application.reseptit.forms import ReseptiForm
 from application.reseptit.models import Resepti
 
 @app.route("/reseptit", methods=["GET"])
@@ -12,36 +12,40 @@ def reseptit_index():
 @app.route("/reseptit/new/")
 @login_required
 def reseptit_form():
-    return render_template("reseptit/new.html", form = TaskForm())
+    return render_template("reseptit/new.html", form = ReseptiForm())
 
 @app.route("/reseptit/<resepti_id>/", methods=["POST"])
 @login_required
 def reseptit_set_cooktime(resepti_id):
-    form = TaskForm(request.form)
-
-    if not form.validate():
-        print("not validated")
-        return redirect(url_for("reseptit_index"))
+    form = ReseptiForm(request.form)
 
     r = Resepti.query.get(resepti_id)   
 
     newCooktime = request.form.get("cooktime")
+    print(newCooktime)
     if newCooktime:
         r.cooktime = newCooktime
+    else:
+        del form.cooktime
         
     newName = request.form.get("name")
     if newName:
         r.name = newName
+    else:
+        del form.name
+
+    if not form.validate():
+        print(form.errors)
+        return redirect(url_for("reseptit_index"))
     
     db.session().commit()
-    print("session committed")
     
     return redirect(url_for("reseptit_index"))
 
 @app.route("/reseptit/", methods=["POST"])
 @login_required
 def reseptit_create():
-    form = TaskForm(request.form)
+    form = ReseptiForm(request.form)
 
     if not form.validate():
         return render_template("reseptit/new.html", form = form)
